6bf1377754772834ac306d07012637a3662c5b05
Dylan Thomas <thomasdylan76@gmail.com>
2020-01-11 09:05:06 -0700

    Updates

---
+++ precache-manifest.a9ace77e66542246bf154a48402dd020.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7204825c288e570918faedb0cdd3cdb",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "b960173fff80aa634d68",
+    "url": "/clicky-game/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ff3518b0ac69294220cd",
+    "url": "/clicky-game/static/css/main.05ecfef1.chunk.css"
+  },
+  {
+    "revision": "b960173fff80aa634d68",
+    "url": "/clicky-game/static/js/2.b7d20e14.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/clicky-game/static/js/2.b7d20e14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff3518b0ac69294220cd",
+    "url": "/clicky-game/static/js/main.1bfb1803.chunk.js"
+  },
+  {
+    "revision": "3e23c9adbd845a404b8c",
+    "url": "/clicky-game/static/js/runtime-main.0abcfe47.js"
+  }
+]);
