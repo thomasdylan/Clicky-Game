86b719fc0ff327c99256d8c3e02e2c38b142d1b3
Dylan Thomas <thomasdylan76@gmail.com>
2020-01-10 23:24:03 -0700

    Back to working

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import TopBar from './components/TopBar';
 import './App.css';
+import TopBar from './components/TopBar';
 import PicCard from './components/PicCard';
-import images from "./images.json"
+import Rules from './components/Rules';
+import images from "./images.json";
 
 class App extends Component {
   constructor() {
@@ -14,54 +15,70 @@ class App extends Component {
     };
   }
 
+  mixUp = array => {
+    array.sort((a, b) => {
+      return .5 - Math.random();
+    });
+  };
+
   increaseScore = () => {
     this.setState({ currentScore: this.state.currentScore + 1 });
-    if(this.state.currentScore >= this.state.highScore) {
+    if (this.state.currentScore >= this.state.highScore) {
       //I was unable to just set highScore to currentScore but this works as intended even when current score is reset.
       this.setState({ highScore: this.state.highScore + 1 });
     };
-    this.forceUpdate();
-  }
-
-  resetScore = () => {
-    this.setState({ currentScore: 0 });
-    for(let i = 0; i <this.state.images.length; i++) {
-      this.state.images.clicked = false;
-    }
-    this.forceUpdate();
-  }
-
-  imageClick = id => {
-    const clicked = this.state.images; 
-    const imageClicked = clicked.filter(images => images.id === id);
-    /* RRRRRRRRRRRRRRRRRREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE */
-    if(!imageClicked.clicked) {
-      this.increaseScore();
-    } else {
+    if (this.state.currentScore === 7) {
+      //I couldn't get react-bootstrap modals to work.
+      alert("You Win!")
       this.resetScore();
     }
   }
 
-  
+  //I know I should never mutate state like this. I'll never do it again after this assignment. Promise.
+  resetScore = () => {
+    const images = this.state.images;
+    for (let i = 0; i < images.length; i++) {
+      images[i].clicked = false;
+    }
+    this.setState({ currentScore: 0 });
+  };
+
+  imageClick = id => {
+    const pic = this.state.images;
+    const wasClicked = pic.filter(image => image.id === id);
+    if (!wasClicked[0].clicked) {
+      wasClicked[0].clicked = true;
+      this.increaseScore();
+      this.mixUp(pic);
+      this.setState({ pic });
+    } else {
+      //I couldn't get react-bootstrap modals to work.
+      alert("You Lose");
+      this.resetScore();
+    }
+  };
+
+
 
   render() {
     return (
       <div>
-        <TopBar 
+        <TopBar
           current={this.state.currentScore}
           high={this.state.highScore}
         />
-        <div className="d-flex justify-content-center main-content mx-auto padding-main flex-wrap row">
-          {images.map(({ id, name, image, clicked }) => (
-            <PicCard 
+        <div className="layout d-flex justify-content-center flex-wrap">
+          {this.state.images.map(({ id, name, image, clicked }) => (
+            <PicCard
               game={this.imageClick}
-              reset={this.resetScore}
               key={id}
+              id={id}
               name={name}
               image={image}
               clicked={clicked}
             />
           ))}
+          <Rules />
         </div>
       </div>
     );
---
+++ clickygame/src/components/Rules.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+const Rules = () => {
+    return (
+        <div className="rules">
+            <h3>Select a picture to gain a point.  Click each picture once and you win, but if you select the same picture twice you lose!</h3>
+        </div>
+    );
+}
+
+export default Rules;
